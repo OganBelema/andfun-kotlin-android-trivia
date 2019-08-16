89cb5a3deb7655b8bb735b036855d6cdade4b399
tamunobelemamiebaka-ogan <belema@softcom.ng>
2019-08-16 21:09:01 +0100

    Prevent drawer from being open in other screens apart from start screen

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -38,6 +38,13 @@ class MainActivity : AppCompatActivity() {
         drawerLayout = binding.drawerLayout
         navController = findNavController(R.id.navHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            if (destination.id == controller.graph.startDestination){
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
         NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
